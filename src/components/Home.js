2abbc01dd03bcfec0559b0f7308e6bd6c75f759c
Martin Convery <mconvery@virtusa.com>
2019-05-22 18:59:39 +0100

    chagnes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from 'react';
+
 import "../styles.css";
 import CountriesList from "./CountriesList";
 
 const Home = () => {
+  const [country, setCountry] = useState(0);
+
   return (
     <main id="home">
       <section className="hero">
@@ -10,7 +13,7 @@ const Home = () => {
           <form id="send-to-form" className="clearfix">
             <div className="custom-select">
               <span>Choose a country to send to</span>
-              <select id="selectTo" name="select-destination">
+              <select id="selectTo" name="select-destination" onChange={(e)=>setCountry(e.value)}>
                 <option value="0">Choose a country to send to</option>
                 <CountriesList />
               </select>
